1fc47dfb2304ad9f7c233d5d35f4e33af5e76d67
mcamilli <mcamilli@42student.it>
2026-01-17 21:04:37 +0100

    triangle

---
+++ practice/main.cpp
@@ -0,0 +1,84 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include "libs/glfw/include/GLFW/glfw3.h"
+
+#ifdef __linux__
+# define GL_GLEXT_PROTOTYPES
+# include <GL/gl.h>
+# include <GL/glu.h>
+#endif
+
+struct AppState
+{
+	float angle;
+};
+
+static void error_callback(int error, const char *description)
+{
+	std::fprintf(stderr, "GLFW error %d: %s\n", error, description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	(void)scancode;
+	(void)mods;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void draw_triangle(void)
+{
+	glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex2f(-0.5f, -0.5f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(0.5f, -0.5f);
+	glEnd();
+}
+
+int main()
+{
+
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+	{
+		std::fprintf(stderr, "Failed to initialize GLFW\n");
+		return EXIT_FAILURE;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "GLFW Triangle", 0, 0);
+	if (!window)
+	{
+		std::fprintf(stderr, "Failed to create GLFW window\n");
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		draw_triangle();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
